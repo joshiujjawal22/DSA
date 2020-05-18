4af0ae43e5376bb5545352852008896d83aba846
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-05-18 23:55:27 +0530

    To find triplets

--- Algorithm/3 sum.java
+++ Algorithm/3 sum.java
@@ -1,18 +1,36 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
+/*nbj
+* 
+*	To find the sum of triplet according to given number
+*	Complexity: O(n*log(n))
+*	Array must be sorted
+*
+*/
+
+
 class threesum{
+
 	public static void main(String args[])
 	{
+
 		Scanner sc =new Scanner(System.in);
+
+		// Length of array
 		int n=sc.nextInt();
 		int a[]=new int[n];
+
 		for(int i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
 		}
+
 		System.out.println("Number to be find");
 		int n_find=sc.nextInt();
-		Arrays.sort(a);
+
+		Arrays.sort(a);	//Sort the given array
+
 		for(int i=0;i<n;i++)
 
 		{	
