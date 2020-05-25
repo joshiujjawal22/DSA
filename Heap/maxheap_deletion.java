c24cabdab4fb1b34ac8fdba770ab4a66d8adb311
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-05-25 23:37:44 +0530

    Deletion in maxheap

---
+++ Heap/maxheap_deletion.java
@@ -0,0 +1,87 @@
+class maxheapusing_insertion
+{
+  // Declaration of Heap
+  static int heap[]=new int[100];
+  static int heapSize = -1;
+  
+  // Insertion 
+  static void insert(int val) 
+  {
+    heapSize++;
+    heap[heapSize] = val; /*Insert in the last place*/
+    int curr = heapSize;
+    while (heap[(curr-1) / 2] < val) 
+    {
+      // Swapping
+
+      int temp=heap[curr];
+      heap[curr] = heap[(curr-1) / 2];
+      heap[(curr-1) / 2]=temp;
+
+      curr = (curr-1) / 2;
+    }   
+  }
+
+  static void delete(){
+    heap[0]=heap[heapSize--];
+    int curr=0;
+    while(curr*2+1<=heapSize || curr*2+2<=heapSize) 
+    {
+      
+        if(heap[curr*2+1]>=heap[curr*2+2] && heap[curr*2+1]>heap[curr]){
+            int temp=heap[curr];
+            heap[curr] = heap[curr*2+1];
+            heap[curr*2+1]=temp;
+            curr=curr*2+1;
+            continue;
+        }
+        else if(heap[curr*2+2]>heap[curr*2+1] && heap[curr*2+2]>heap[curr]){
+            int temp=heap[curr];
+            heap[curr] = heap[curr*2+2];
+            heap[curr*2+2]=temp;
+            curr=curr*2+2;
+            continue;
+        }
+        else break;
+     
+     }       
+  }
+
+   public static void main(String args[]) 
+  {
+
+    int i;
+    insert(10);
+    insert(40);
+    insert(15);
+    System.out.println("Heap array after inserting 10, 40, 15 elements :");
+    for (i = 0; i < heapSize+1; i++)
+      System.out.print(heap[i]+" ");
+   System.out.println();
+
+    delete();
+    System.out.println("Heap array after deletion");
+    for (i = 0; i < heapSize+1; i++)
+      System.out.print(heap[i]+" ");
+   System.out.println();
+
+    insert(50);
+    System.out.println("Heap array after inserting 50");
+    for (i = 0; i < heapSize+1; i++)
+      System.out.print(heap[i]+" ");
+   System.out.println();
+
+    delete();
+    System.out.println("Heap array after deletion");
+    for (i = 0; i < heapSize+1; i++)
+      System.out.print(heap[i]+" ");
+   System.out.println();
+    
+
+    
+    insert(20);
+    System.out.println("Heap array after inserting 20");
+    for (i = 0; i < heapSize+1; i++)
+      System.out.print(heap[i]+" ");
+}
+}
