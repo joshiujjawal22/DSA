1a128533eb9fd3c20e09090e70dc695032350842
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-05-26 00:09:01 +0530

    minheap deletion

---
+++ Heap/minheap_deletion.java
@@ -0,0 +1,77 @@
+class minheap_deletion
+{
+  // Declaration of Heap
+  static int heap[]=new int[100];
+  static int heapSize = -1;
+
+  // Insertion
+  static void insert(int val) 
+  {
+    heapSize++;
+    heap[heapSize] = val; /*Insert in the last place*/
+    int curr = heapSize;
+    while (heap[(curr-1) / 2] > val) 
+    {
+
+      // Swapping
+
+      int temp=heap[curr];
+      heap[curr] = heap[(curr-1) / 2];
+      heap[(curr-1) / 2]=temp;
+
+      curr = (curr-1) / 2;
+    }
+
+  }
+
+  static void delete(){
+    heap[0]=heap[heapSize--];
+    int curr=0;
+
+    // Swapping of value according to greater value
+    
+    while(curr*2+1<=heapSize || curr*2+2<=heapSize) 
+    {
+        
+        if(heap[curr*2+1]<=heap[curr*2+2] && heap[curr*2+1]<heap[curr]){
+            int temp=heap[curr];
+            heap[curr] = heap[curr*2+1];
+            heap[curr*2+1]=temp;
+            curr=curr*2+1;
+            continue;
+        }
+
+        else if(heap[curr*2+2]<heap[curr*2+1] && heap[curr*2+2]<heap[curr]){
+            int temp=heap[curr];
+            heap[curr] = heap[curr*2+2];
+            heap[curr*2+2]=temp;
+            curr=curr*2+2;
+            continue;
+        }
+        else break;
+     
+     }       
+  }
+
+
+   public static void main(String ab[]) 
+  {
+    int i;
+    insert(10);
+    insert(5);
+    insert(15);
+    delete();
+
+    System.out.println("Heap array after inserting 10, 5, 15 elements : ");
+    for (i = 0; i < heapSize+1; i++)
+      System.out.print(heap[i]+" ");
+   System.out.println();
+
+
+    insert(4);
+    System.out.println("Heap array after inserting 10, 5, 15, 4 elements : ");
+    for (i = 0; i < heapSize+1; i++)
+      System.out.print(heap[i]+" ");
+}
+
+}
