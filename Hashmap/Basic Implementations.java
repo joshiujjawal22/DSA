65d5efe4f4b8383749563f0bba2fb2f7c1bcb389
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-08-02 00:27:24 +0530

    Implementation

--- Hashmap/Basic Implementations.java
+++ Hashmap/Basic Implementations.java
@@ -1,6 +1,8 @@
 /*Program to illustrate the 
 * basic implementation of
-* hashmap i.e put,get,containskey
+* hashmap i.e put(),get(),
+* containskey(),isEmpty(),
+* size()
 */
 
 // Library used to import hashmap.
@@ -13,17 +15,39 @@ public class HashMap_Implementation {
 		HashMap<String, String> map 
 			= new HashMap<>(); 
 
-		// Put function to add ("Keys","Values") in hashmap
+		// To check the hashmap 
+		// is empty or not
+		// using isEmpty() function
+		System.out.println("If map is empty it returns "+map.isEmpty());
+
+		// Put function to add 
+		// ("Keys","Values") in hashmap
 		map.put("Ujjawal", "Joshi"); 
 		map.put("Ankur", "Lohiya"); 
 		map.put("Chetan", "Batra"); 
 
 
-		// To print size of hashmap using size() function
+		// To print size of hashmap 
+		// using size() function
 		System.out.println("Size of map is:- "+ map.size()); 
 
 		// To print content of hashmap 
 		System.out.println("Map in format of (Keys=Values):\n"+map);
+
+		// To check the hashmap 
+		// is empty or not 
+		// using isEmpty() function
+		System.out.println("If map is not empty it returns "+map.isEmpty());
+
+		// To get the value using get("keys") function:
+		System.out.println(map.get("Ujjawal"));
+
+		// To check whether the hashmap contains a key or not
+		// Using containsKey("Value")
+		// if key is found it returns true else false
+		System.out.println(map.containsKey("Joshi"));
+
+
 		
 		
 	} 
